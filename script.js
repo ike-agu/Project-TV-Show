fb0618d72dc4fb990b4bc4b89359bbbd517d0e9b
ike-agu <ikennaww@yahoo.fr>
2025-08-03 07:36:45 +0100

    created a helper function that creates and append Html elements to the parent element

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ function makePageForEpisodes(episodeList) {
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
+// Helper function. Creates and appends HTML element to a parent element e.g <h3>, <p>, <img> etc
+function episodeCard(tagName, textContent, src, parentElement) {
+  const element = document.createElement(tagName);
+  element.textContent = textContent;
+  if (tagName === "img") {
+    element.src = src || textContent;
+    element.alt = "Episode Image";
+  } else {
+    element.textContent = textContent;
+  }
+  parentElement.appendChild(element);
+  return element;
+}
+
 window.onload = setup;
