e815ba5218af18346c558b9257dfcfe933389eb9
ike-agu <ikennaww@yahoo.fr>
2025-08-03 09:24:02 +0100

    created and implemented all episode card fucntion. created for..of loop to display all episode cards

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  
+ const allEpisodesList = [];
+ for (const item of episodeList) {
+   allEpisodesList.push(allEpisodesCard(item));
+ }
+ rootElem.append(...allEpisodesList )
 }
 
-// Helper function. Creates and appends HTML element to a parent element e.g <h3>, <p>, <img> etc
+
+
 function episodeCard(tagName, textContent, src, parentElement) {
   const element = document.createElement(tagName);
   element.textContent = textContent;
@@ -22,5 +31,18 @@ function episodeCard(tagName, textContent, src, parentElement) {
   parentElement.appendChild(element);
   return element;
 }
+
+function allEpisodesCard(episode){
+  const cardForEpisodes = document.createElement("section")
+  episodeCard("img", `${episode.image.medium}`,  episode.image.medium,  cardForEpisodes);
+  episodeCard("h3", `Episode: ${episode.name}`, null,  cardForEpisodes);
+  episodeCard("p", `Season: ${episode.season}`,null, cardForEpisodes);
+  episodeCard("p", `Episode Number: ${episode.number}`, null, cardForEpisodes);
+  episodeCard("p", `Summary: ${episode.summary}`, null, cardForEpisodes);
+
+   return cardForEpisodes;
+}
+
+
 
 window.onload = setup;
