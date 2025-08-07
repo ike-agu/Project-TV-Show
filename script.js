b07a1b7c6cdbe3ea808b815d7b46987565bd31c5
Anuar Duisenbek <coolboy949494@gmail.com>
2025-08-08 00:27:50 +0100

    add dynamic select dropdown for episodes and  implement live search for episodes,prevent multiple footers, refactor footer creation

--- script.js
+++ script.js
@@ -2,17 +2,74 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-}
 
-function makePageForEpisodes(episodeList) {
+  //create a container for controls
+  const controlsContainer = document.createElement("div");
+  controlsContainer.className = "controls-container";
+
+  // Create dynamic select dropdown for episodes
+  const selectElem = document.createElement("select");
+  selectElem.id = "selected-episode";
+  selectElem.name = "episodes";
+
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "Select an episode";
+  selectElem.appendChild(defaultOption);
+
+  controlsContainer.appendChild(selectElem);
+
+  //create a live search input for episodes
+
+  const searchInput = document.createElement("input");
+  searchInput.type = "text";
+  searchInput.id = "search";
+  searchInput.name = "search";
+  searchInput.placeholder = "Search term";
+
+  controlsContainer.appendChild(searchInput);
+
   const rootElem = document.getElementById("root");
-  const episodesContainer = document.createElement("div");
-  episodesContainer.classList.add("episodes-container");
+  document.body.insertBefore(controlsContainer, rootElem);
 
-  //displays all episode cards
-  const allEpisodesList = episodeList.map(allEpisodesCard)
-  rootElem.append(...allEpisodesList);
+  allEpisodes.forEach((episode) => {
+    const option = document.createElement("option");
+    option.value = episode.id;
+    option.textContent = `S${String(episode.season).padStart(2, "0")}
+    E${String(episode.number).padStart(2, "0")} - ${episode.name}`;
 
+    selectElem.appendChild(option);
+  });
+
+  selectElem.addEventListener("change", (event) => {
+    searchInput.value = "";
+    const selectedEpisodeValue = event.target.value;
+
+    if (selectedEpisodeValue === "") {
+      makePageForEpisodes(allEpisodes);
+      return;
+    } else {
+      const selectedEpisode = allEpisodes.find(
+        (episode) => episode.id === parseInt(selectedEpisodeValue, 10)
+      );
+      if (selectedEpisode) {
+        makePageForEpisodes([selectedEpisode]);
+      }
+    }
+  });
+
+  searchInput.addEventListener("input", (event) => {
+    selectElem.value = "";
+    const searchTerm = event.target.value.toLowerCase();
+    const filteredEpisodes = allEpisodes.filter(
+      (episode) =>
+        episode.summary.toLowerCase().includes(searchTerm) ||
+        episode.name.toLowerCase().includes(searchTerm) ||
+        episode.season.toString().includes(searchTerm)
+    );
+
+    makePageForEpisodes(filteredEpisodes);
+  });
   // 4. Create footer
   const footer = document.createElement("footer");
 
@@ -21,10 +78,22 @@ function makePageForEpisodes(episodeList) {
   sourceLink.textContent = "TV Shows and Series are from TVMaze.com";
   sourceLink.href = "https://www.tvmaze.com/";
   sourceLink.target = "_blank";
-  sourceLink.ref = "noopener noreferrer";
+  sourceLink.rel = "noopener noreferrer";
 
   footer.appendChild(sourceLink);
   document.body.append(footer);
+}
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.innerHTML = "";
+
+  const episodesContainer = document.createElement("div");
+  episodesContainer.classList.add("episodes-container");
+
+  //displays all episode cards
+  const allEpisodesList = episodeList.map(allEpisodesCard);
+  rootElem.append(...allEpisodesList);
 }
 
 // Helper function. Creates and appends HTML element to a parent element e.g <h3>, <p>, <img> etc
@@ -47,9 +116,15 @@ function allEpisodesCard(episode) {
 
   const seasonNumber = `${episode.season}`;
   const episodeNumber = `${episode.number}`;
-  const episodeCode = "S" + seasonNumber.padStart(2, 0) + "E" + episodeNumber.padStart(2, 0);//create Episode Code
+  const episodeCode =
+    "S" + seasonNumber.padStart(2, 0) + "E" + episodeNumber.padStart(2, 0); //create Episode Code
 
-  episodeCard("img",`${episode.image.medium}`, episode.image.medium, cardForEpisodes);
+  episodeCard(
+    "img",
+    `${episode.image.medium}`,
+    episode.image.medium,
+    cardForEpisodes
+  );
   episodeCard("h3", `${episode.name}`, null, cardForEpisodes);
   episodeCard("p", `${episodeCode}`, null, cardForEpisodes);
   episodeCard("p", `Summary: ${episode.summary}`, null, cardForEpisodes);
